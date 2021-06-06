9c3b3cdb31afad5d748c5af7dd54dec17c48479d
ahinkson97 <ahinkson@aggies.ncat.edu>
2021-06-05 20:48:23 -0700

    creation of questions as variables

---
+++ script.js
@@ -0,0 +1,7 @@
+var q1 = ["What does CSS stand for?"]
+var q2 = ["What is an array?"]
+var q3 = ["What are three coding languages?"]
+var q4 = ["What is a boolean?"]
+var q5 = ["How does a server function?"]
+
+

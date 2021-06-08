d2aa6fcdab9fb33214401e15dc3ec2034b83aa81
ahinkson97 <ahinkson@aggies.ncat.edu>
2021-06-07 23:55:08 -0400

    created answers to questions

--- assets/script.js
+++ assets/script.js
@@ -28,10 +28,16 @@ function countdown() {
 }
 // create questions
 var q1 = ["What does CSS stand for?"]
-var q2 = ["What is an array?"]
+var q2 = ["True or False, an array is an object."]
 var q3 = ["What are three coding languages?"]
-var q4 = ["What is a boolean?"]
-var q5 = ["How does a server function?"]
+var q4 = ["Select three Primitve types."]
+var q5 = ["What does DRY mean?"]
+
+var a1 = ["Cute Smart Styles, Cascading Silly Sheets, Cascading Style Sheets"]
+var a2 = ["True"]
+var a3 = ["CSS, Roblox, HTML, iMessage, JavaScript,"]
+var a4 = ["Boolean, String, Number, Understand, Variable"]
+var a5 = ["Don't Recognize Yourself, Don't Reuse Yeast, Don't Repeat Yourself"]
 
 // when question is answered correctly, presented with another question
 
